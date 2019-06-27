4f5b4becd29f332e42fca8a4f73c963a34b56271
Rohan Samavedam <rohansamavedam@gmail.com>
2019-06-27 20:03:15 +0530

    weather and travel advisory added

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,46 @@ const validateEntry = () => {
   return isValid;
 }
 
+function myFunction(val) {
+  var d = new Date(val)
+  var n = d.getDate();
+  var k = d.getFullYear();
+  var p = d.getMonth();
+  return  p + '/' + n + '/' + k;
+}
+
+function travelVer(val){
+  if(val<2){
+    $('#travelinfo').text('Travel is usually safe')
+  }else if(val>2 && val<3.5){
+    $('#travelinfo').text('Travel with higher caution')
+  }else if(val>3.5 && val<4.5){
+    $('#travelinfo').text('Reconsider travelling')
+  }else{
+    $('#travelinfo').text('Do not travel')
+  }
+}
+
 $(document).ready(function() {
+  var myDateOne = myFunction( $('#onetime').text() *1000);
+  var myDateTwo = myFunction( $('#twotime').text() *1000);
+  var myDateThree = myFunction( $('#threetime').text() *1000);
+  var myDateFour = myFunction( $('#fourtime').text() *1000);
+  var myDateFive = myFunction( $('#fivetime').text() *1000);
+
+  $('#onetime').text(myDateOne)
+  $('#twotime').text(myDateTwo)
+  $('#threetime').text(myDateThree)
+  $('#fourtime').text(myDateFour)
+  $('#fivetime').text(myDateFive)
+  var val = $('#travelinfo').text()
+  travelVer(val);
+  console.log(val);
+
+
+
+
+
     $('#imgholder').hide();
     $('#down').hide();
     var pathname = window.location.pathname
--- src/app.js
+++ src/app.js
@@ -57,7 +57,31 @@ app.get('/info', (req, res) => {
                     summary: weatherData.summary,
                     dayOneTempMax: weatherData.entireData[0].temperatureMax,
                     dayOneTempMin: weatherData.entireData[0].temperatureMin,
+
+                    dayTwoTempMax: weatherData.entireData[1].temperatureMax,
+                    dayTwoTempMin: weatherData.entireData[1].temperatureMin,
+
+                    dayThreeTempMax: weatherData.entireData[2].temperatureMax,
+                    dayThreeTempMin: weatherData.entireData[2].temperatureMin,
+
+                    dayFourTempMax: weatherData.entireData[3].temperatureMax,
+                    dayFourTempMin: weatherData.entireData[3].temperatureMin,
+
+                    dayFiveTempMax: weatherData.entireData[4].temperatureMax,
+                    dayFiveTempMin: weatherData.entireData[4].temperatureMin,
+
                     iconOne: weatherData.entireData[0].icon,
+                    iconTwo: weatherData.entireData[1].icon,
+                    iconThree: weatherData.entireData[2].icon,
+                    iconFour: weatherData.entireData[3].icon,
+                    iconFive: weatherData.entireData[4].icon,
+
+                    oneTime: weatherData.entireData[0].time,
+                    twoTime: weatherData.entireData[1].time,
+                    threeTime: weatherData.entireData[2].time,
+                    fourTime: weatherData.entireData[3].time,
+                    fiveTime: weatherData.entireData[4].time,
+
                     picdata1: picdata[0].src.large,
                     picdata2: picdata[1].src.large,
                     picdata3: picdata[2].src.large,
@@ -67,6 +91,7 @@ app.get('/info', (req, res) => {
                     picdata7: picdata[6].src.large,
                     picdata8: picdata[7].src.large,
                     picdata9: picdata[8].src.large,
+
                     traveladData: traveladData.advisory.score,
                 })
 
